0c071d4eec777cbd3463464b828c831e47932731
tomas <tomasmed@umich.edu>
2015-03-20 20:33:44 -0400

    Isometric base test

---
+++ Assets/Room_S.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room_S : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.tag == "a")print ("tag is a");
+		print ("Someone is in the room\n");
+	}
+
+     
+}
