7de1fe48843ba6c181ae1fcf75cb2f515c656e7c
gatriex <haasjac@gmail.com>
2015-03-20 21:50:48 -0700

    Key controls
    arrow keys steve
    wasd bob

--- Assets/Scripts/Bob.cs
+++ Assets/Scripts/Bob.cs
@@ -6,21 +6,26 @@ public class Bob : MonoBehaviour {
 	public string Horizontal = "L_XAxis_2";
 	public string Vertical = "L_YAxis_2";
 	public string B_button = "B_2";
+	public string X_button = "X_2";
 	public float leap_force = 30f;
 	public float run_speed = 15f;
 	public float jump_time = 0.1f;
 	bool jumping = false;
 	float jump_clock = 0;
 	Vector3 tempjump = new Vector3(0,0,0);
+	public GameObject Steve;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		Steve = GameObject.Find ("Bob");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetButton (Horizontal)) {
+			Debug.Log (Input.GetButton (Horizontal));
+		}
 		if (jumping) {
 			GetComponent<Rigidbody> ().velocity = (tempjump * leap_force);
 			jump_clock += Time.deltaTime;
@@ -36,6 +41,11 @@ public class Bob : MonoBehaviour {
 			tempjump.Normalize();
 			jumping = true;
 			jump_clock = 0;
+		}
+		if (Input.GetButtonDown (X_button)) {
+			Vector3 temppos = transform.position;
+			transform.position = Steve.transform.position;
+			Steve.transform.position = temppos;
 		}
 	}
 }
