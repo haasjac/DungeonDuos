b9847f335c4ccc98df8a125c1de20d989404fcae
tomas <tomasmed@umich.edu>
2015-03-21 13:11:28 -0400

    Added Fire to torches

--- Assets/Room_S.cs
+++ Assets/Room_S.cs
@@ -7,18 +7,31 @@ public class Room_S : MonoBehaviour {
 	public Material Trans;
 	public Material Opaque;
 
+	public GameObject Torch1;
+	public GameObject Torch2;
+	public GameObject Torch3;
+	public GameObject Torch4;
+
 	void OnTriggerEnter (Collider other)
 	{
 		if (other.tag == "Player1") {
 			Wall1.GetComponent<Renderer> ().material = Trans; 
 			Wall2.GetComponent<Renderer> ().material = Trans; 
+			Torch1.GetComponentInChildren<ParticleSystem>().Play();
+			Torch2.GetComponentInChildren<ParticleSystem>().Play();
+			Torch3.GetComponentInChildren<ParticleSystem>().Play();
+			Torch4.GetComponentInChildren<ParticleSystem>().Play();
 		}   
 	}
 	void OnTriggerExit (Collider other)
 	{
 		if (other.tag == "Player1") {
 			Wall1.GetComponent<Renderer> ().material = Opaque; 
-			Wall2.GetComponent<Renderer> ().material = Opaque; 
+			Wall2.GetComponent<Renderer> ().material = Opaque;
+			Torch1.GetComponentInChildren<ParticleSystem>().Stop();
+			Torch2.GetComponentInChildren<ParticleSystem>().Stop();
+			Torch3.GetComponentInChildren<ParticleSystem>().Stop();
+			Torch4.GetComponentInChildren<ParticleSystem>().Stop();
 		}
 	}
 }
