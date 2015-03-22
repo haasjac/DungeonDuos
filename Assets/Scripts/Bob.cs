00ced07735f792adb80da0626b255abf9400fccd
gatriex <haasjac@gmail.com>
2015-03-22 03:02:53 -0400

    Combat fix and texturing
    hurdle, text box.
    gameobject no longer bad access after enemy dies

--- Assets/Scripts/Bob.cs
+++ Assets/Scripts/Bob.cs
@@ -94,17 +94,20 @@ public class Bob : MonoBehaviour {
 			//Debug.Log("ATTACK");
             GameObject.Find("Swoosh" + Random.Range(1, 4)).GetComponent<AudioSource>().Play();
 			foreach (GameObject item in enemies) {
-                Vector3 direction = item.transform.position - this.transform.position;
-                float distA = direction.magnitude;
-                direction = direction / distA;
-				distA = Vector3.Distance(item.transform.position, this.transform.position);
-				//Debug.Log(distA);
-				if (distA < damage_range) {
-					//Debug.Log(item);
-                    if (Mathf.Abs(Vector3.Angle(vel, direction)) <= 70)
-                    {
-                        item.GetComponent<enemyHealth>().changeHealth((-1 * damage));
-                    }
+				if (item != null) {
+	                Vector3 direction = item.transform.position - this.transform.position;
+	                float distA = direction.magnitude;
+	                direction = direction / distA;
+					distA = Vector3.Distance(item.transform.position, this.transform.position);
+					//Debug.Log(distA);
+					if (distA < damage_range) {
+						//Debug.Log(item);
+	                    if (Mathf.Abs(Vector3.Angle(vel, direction)) <= 70)
+	                    {
+	                        item.GetComponent<enemyHealth>().changeHealth((-1 * damage));
+							//enemies = GameObject.FindGameObjectsWithTag("EnemyEnemy");
+	                    }
+					}
 				}
 			}
 		}
