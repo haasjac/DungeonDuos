279c1239595a17c2a8364e9db5e353d9d081b4a6
gatriex <haasjac@gmail.com>
2015-03-22 03:50:03 -0400

    Level stuff

---
+++ Assets/Menu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public string Next_Level;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Submit")) {
+			Application.LoadLevel(Next_Level);
+		}
+	}
+}
