9976414d9e9ff9f8c60e2ee6f8b621c7218dfdeb
gatriex <haasjac@gmail.com>
2015-03-21 21:40:58 -0400

    Death diaglouge level 2

--- Assets/Room_S.cs
+++ Assets/Room_S.cs
@@ -10,7 +10,7 @@ public class Room_S : MonoBehaviour {
 	//public GameObject Torch1;
 	//public GameObject Torch2;
 	//public GameObject Torch3;
-	//ublic GameObject Torch4;
+	//public GameObject Torch4;
 
 	void OnTriggerStay (Collider other)
 	{
@@ -18,10 +18,10 @@ public class Room_S : MonoBehaviour {
 			GameObject.Find("Transition").GetComponent<AudioSource>().Play();
 			Wall1.GetComponent<Renderer> ().material = Trans; 
 			Wall2.GetComponent<Renderer> ().material = Trans; 
-			Torch1.GetComponentInChildren<ParticleSystem>().Play();
-			Torch2.GetComponentInChildren<ParticleSystem>().Play();
-			Torch3.GetComponentInChildren<ParticleSystem>().Play();
-			Torch4.GetComponentInChildren<ParticleSystem>().Play();
+			//Torch1.GetComponentInChildren<ParticleSystem>().Play();
+			//Torch2.GetComponentInChildren<ParticleSystem>().Play();
+			//Torch3.GetComponentInChildren<ParticleSystem>().Play();
+			//Torch4.GetComponentInChildren<ParticleSystem>().Play();
 		}   
 	}
 	void OnTriggerExit (Collider other)
@@ -29,10 +29,10 @@ public class Room_S : MonoBehaviour {
 		if (other.tag == "Player1" || other.tag == "Player2") {
 			Wall1.GetComponent<Renderer> ().material = Opaque; 
 			Wall2.GetComponent<Renderer> ().material = Opaque;
-			Torch1.GetComponentInChildren<ParticleSystem>().Stop();
-			Torch2.GetComponentInChildren<ParticleSystem>().Stop();
-			Torch3.GetComponentInChildren<ParticleSystem>().Stop();
-			Torch4.GetComponentInChildren<ParticleSystem>().Stop();
+			//Torch1.GetComponentInChildren<ParticleSystem>().Stop();
+			//Torch2.GetComponentInChildren<ParticleSystem>().Stop();
+			//Torch3.GetComponentInChildren<ParticleSystem>().Stop();
+			//Torch4.GetComponentInChildren<ParticleSystem>().Stop();
 		}
 	}
 }
